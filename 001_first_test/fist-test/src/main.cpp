7c37d47c186368c1e4541720d72ed3cf33b4f6ba
manuelkleinert <manuel.kleinert@gmail.com>
2020-09-24 15:02:36 +0200

    update

--- 001_first_test/fist-test/src/main.cpp
+++ 001_first_test/fist-test/src/main.cpp
@@ -1,12 +1,12 @@
 #include <Arduino.h>
 // #include <Stepper.h>
-#include <LiquidCrystal.h>
+// #include <LiquidCrystal.h>
 
-int SPU = 2048; // Schritte pro Umdrehung.
+// int SPU = 2048; // Schritte pro Umdrehung.
 // Stepper Motor(SPU, 3,5,4,6); // Der Schrittmotor erhält die Bezeichnung "Motor" und es wird angegeben an welchen Pins der Motor angeschlossen ist.
 
 
-LiquidCrystal lcd(8, 9, 10, 11, 12, 13);
+// LiquidCrystal lcd(8, 9, 10, 11, 12, 13);
 // LiquidCrystal lcd(13, 12, 11, 10, 9, 8);
 
 // int Led_Rot = 2; //select the pin for the red LED
@@ -15,21 +15,43 @@ LiquidCrystal lcd(8, 9, 10, 11, 12, 13);
 //
 // int val;
 
+const int ledPin = 13;
+int incomingByte;
+
 void setup() {
+  Serial.begin(9600);
+  pinMode(ledPin, OUTPUT);
+
   // pinMode(Led_Rot, OUTPUT);
   // pinMode(Led_Blau, OUTPUT);
   // pinMode(Led_Gruen, OUTPUT);
   // Serial.begin(9600);
 
-  lcd.begin(16, 2);
-  lcd.print("Hallo Joerg");
-  lcd.setCursor(0,1);
-  lcd.print("Bis am Mittag");
+  // lcd.begin(16, 2);
+  // lcd.print("Hallo Joerg");
+  // lcd.setCursor(0,1);
+  // lcd.print("Bis am Mittag");
 
   // Motor.setSpeed(10);
 }
 
 void loop() {
+  // Serial.println("Hello world from Ardunio!"); // write a string
+
+
+  if (Serial.available() > 0) {
+      // read the oldest byte in the serial buffer:
+      incomingByte = Serial.read();
+      // if it's a capital H (ASCII 72), turn on the LED:
+      if (incomingByte == 'H') {
+        digitalWrite(ledPin, HIGH);
+      }
+      // if it's an L (ASCII 76) turn off the LED:
+      if (incomingByte == 'L') {
+        digitalWrite(ledPin, LOW);
+      }
+    }
+
   // digitalWrite(2, 1);
   // digitalWrite(3, 0);
   // digitalWrite(4, 0);
